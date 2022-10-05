6f08794827572bd1a66e1b8ba7ec6b1fb3d518d4
stargazer741 <102627303+stargazer741@users.noreply.github.com>
2022-10-05 20:39:15 +0300

    improvements (#6)
    * login page
    
    * left menu
    
    * message window
    
    * fix
    
    * improvements

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const inputNickname = document.querySelector("#inputNickname");
 const btnLogin = document.querySelector("#btnLogin");
+const chatElements = document.querySelectorAll(".chat");
+const chatBar = document.querySelector("#chat-screen");
 
-
+chatElements.forEach((chat) => {
+  chat.addEventListener("click", () => {
+    chatBar.classList.toggle("active");
+  });
+});
